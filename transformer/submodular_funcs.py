7e77e07a10daff3e4a4cdbb8f3357effecd28e90
xnfan <838721201@qq.com>
2020-05-16 20:59:16 +0800

    Signed-off-by: xnfan <838721201@qq.com>

---
+++ transformer/submodular_funcs.py
@@ -0,0 +1,286 @@
+import numpy as np
+import scipy.linalg as la
+import pdb
+from nltk import ngrams
+import difflib
+import pickle
+from time import time
+import os
+
+from scipy.spatial.distance import pdist, squareform
+import scipy
+from numpy import dot
+from numpy.linalg import norm
+
+#####################################################################################################################
+
+########################################### RBF QUALITY FUNCTION ####################################################
+
+embedding_path = os.path.join('./data', 'embeddings', 'quora_word2vec.pickle')
+model = None
+
+if os.path.exists(embedding_path):
+    print('Loading Word2Vec')
+    st_time = time()
+
+    with open(embedding_path, 'rb') as f:
+        model = pickle.load(f)
+
+    print('Word2vec Loaded')
+    etime = (time() - st_time)/60.0
+    print('Time Taken : {}'.format(etime))
+    
+cos_sim = lambda a,b: dot(a, b)/(norm(a)*norm(b))
+rbf = lambda a,b, sigma : scipy.exp(-(np.sum( (a-b)**2 ) )/ sigma ** 2)
+
+def sent2wvec(s):
+    v= []
+    for w in s:
+        try:
+            vec =  model[w]
+            v.append(vec)
+        except:
+            vec = np.random.random(256)
+            v.append(vec)
+
+    v = np.array(v)
+    return v
+
+def sentence_compare(s1, s2, kernel='cos', **kwargs):
+    l1 = s1[0].split()
+    l2 = s2.split()
+
+    v1= sent2wvec(l1)
+    v2= sent2wvec(l2)
+    # v2 = np.array([model.wv.word_vec(w) for w in l2])
+    score = 0
+    len_s1 = v1.shape[0]
+    for v in v1:
+        if kernel == 'cos':
+            wscore = np.max(np.array([cos_sim(v,i) for i in v2] ))
+        elif kernel == 'rbf':
+            wscore = np.max(np.array([rbf(v,i, kwargs['sigma']) for i in v2] ))
+        else:
+            print('Error in kernel type')
+        score += wscore/len_s1
+
+    return score
+
+
+def similarity_func(v, S):
+    if len(S):
+        score = 0.0
+
+        for sent in S:
+            score+= sentence_compare(v[0], sent, kernel='cos', sigma=1.0)
+
+        return np.sqrt(score)
+    else:
+        return 0.0
+
+
+def similarity_gain(v, s, base_score=0.0):
+    score = 0.0
+    score+= sentence_compare(v, s, sigma=1.0)
+    score += (base_score**2)
+
+    return np.sqrt(score)
+
+
+#####################################################################################################################
+#####################################################################################################################
+
+########################################### NGRAM FUNCTIONS #########################################################
+
+
+
+def ngram_toks(sents, n=1):
+    ntoks =[]
+    for sent in sents:
+        ntok = list(ngrams(sent.split(), n))
+        newtoks = [tok for tok in ntok]
+        ntoks+= newtoks
+    return ntoks
+
+def distinct_ngrams(S):
+    if len(S):
+        S = ' '.join(S)
+        N = [1,2,3]
+        score = 0.0
+        for n in N:
+            toks = set(ngram_toks([S], n))
+            score+= (1.0/n) * len(toks)
+
+        return score
+    else:
+        return 0.0
+
+def distinct_src_ngrams_func(v, S):
+    #print(S)
+    if len(S):
+        S = ' '.join(S)
+        N = [1,2,3]
+        S = S + ' ' + v[0]
+        score = 0.0
+        for n in N:
+            src_tokens = len(ngram_toks(v, n))-1
+            toks = set(ngram_toks([S], n))
+            score+= (1.0/n) * (len(toks) - src_tokens)
+
+        return np.sqrt(score)
+    else:
+        return 0.0
+
+def distinct_src_ngrams_gain(v, s, S, base_score = 0.0):
+    S = [' '.join(x) for x in S]
+    #print(S)
+    if len(S):
+        S = ' '.join(S)
+        N = [1,2,3]
+        S = S + ' ' + v[0] + s
+        gain = 0.0
+        for n in N:
+            src_tokens = len(ngram_toks(v, n))
+            toks = set(ngram_toks([S], n))
+            gain+= (1.0/n) * (len(toks) - src_tokens)
+
+        return np.sqrt((base_score**2) + gain)
+    else:
+        return 0.0
+
+def ngram_overlap(v, S):
+    if len(S):
+        N = [1,2,3]
+        score = 0.0
+
+
+        for n in N:
+            src_toks = set(ngram_toks(v, n))
+            for sent in S:
+                sent_toks = set(ngram_toks(S, n))
+
+                overlap = src_toks.intersection(sent_toks)
+                loverlap = min(len(overlap), 0.7*len(v[0].split()))
+                score += (1.0/(4-n)) * loverlap
+
+        return np.sqrt(score)
+    else:
+        return 0.0
+
+def ngram_overlap_unit(v, S , base_score=0.0):
+    N = [1,2,3]
+    score = 0.0
+    try:
+        temp = S[0]
+    except:
+        S = [S]
+
+    for n in N:
+        src_toks = set(ngram_toks(v, n))
+        sent_toks = set(ngram_toks([S], n))
+        overlap = src_toks.intersection(sent_toks)
+
+        loverlap = min(len(overlap), 0.7*len(src_toks))
+
+        score += (1.0/(4-n)) * loverlap
+
+    return np.sqrt( (base_score**2) + score)
+
+
+#####################################################################################################################
+
+########################################### EDIT DISTANCE FUNCTION ##################################################
+
+def seq_func(V, S):
+    if len(S):
+        score = 0.0
+        for v in V:
+            for s in S:
+                vx = v.split()
+                sx = s.split()
+
+                seq = difflib.SequenceMatcher(None, vx, sx)
+                score+= seq.ratio()
+
+        return np.sqrt(score)
+    else:
+        return 0.0
+
+def seq_gain(V, s, base_score= 0.0):
+    gain =0.0
+    for v in V:
+        vx = v.split()
+        sx = s.split()
+
+        seq = difflib.SequenceMatcher(None, vx, sx)
+        gain+= seq.ratio()
+
+    score =(base_score**2) + gain
+
+    return np.sqrt(score)
+
+def info_func(S, orig_count, ref_count):
+    if len(S):
+        score =0.0
+        for s in S:
+            stoks = set(s.split())
+            orig_toks = set(orig_count.keys())
+
+            int_toks = stoks.intersection(orig_toks)
+            for tok in int_toks:
+                try:
+                    score+= (orig_count[tok] / (1 + ref_count[tok] ))
+                except:
+                    score+= orig_count[tok]
+
+        return np.sqrt(score)
+
+    else:
+        return 0.0
+
+def info_gain(s, orig_count, ref_count, base_score =0.0):
+    score =0.0
+    stoks = set(s.split())
+    orig_toks = set(orig_count.keys())
+
+    int_toks = stoks.intersection(orig_toks)
+    for tok in int_toks:
+        try:
+            score+= (orig_count[tok] / (1 + ref_count[tok] ))
+        except:
+            score+= orig_count[tok]
+
+    score+= (base_score**2)
+
+    return np.sqrt(score)
+
+
+def jkd_func(V, S):
+    if len(S):
+        score = 0.0
+        for v in V:
+            v_set = set(v)
+            for s in S:
+                s_set = set(s)
+                and_set = s_set.intersection(v_set)
+                or_set = s_set.union(v_set)
+                score_temp = 1 - float(len(and_set)) / len(or_set)
+                score += score_temp
+
+        return np.sqrt(score)
+    else:
+        return 0.0
+
+def jkd_gain(V, s, base_score= 0.0):
+    gain =0.0
+    s_set = set(s)
+    for v in V:
+        v_set = set(V)
+        and_set = s_set.intersection(v_set)
+        or_set = s_set.union(v_set)
+        score_temp = 1 - float(len(and_set)) / len(or_set)
+        gain += score_temp
+
+    score =(base_score**2) + gain
+
+    return np.sqrt(score)
